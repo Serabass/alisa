ee2fd0aa7342115b33f25a743cebced2084aaac1
Serabass <serabass565@gmail.com>
2022-04-05 22:33:41 +0600

    upd

--- log.php
+++ log.php
@@ -21,26 +21,29 @@ function getLog()
 
 $log = getLog();
 
-?>
-
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Logs</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
 </head>
 <body>
   
-  <ul>
+  <ul class="list-group">
     <?php foreach ($log as $row) { ?>
-      <li>
+      <li class="list-group-item">
         <b> <?php echo $row['time'] ?> </b>
         <span> <?php echo $row['text']['request']['command'] ?> </span>
       </li>
     <?php } ?>
   </ul>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
