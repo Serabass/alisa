a74c46c562a3c33a5052d02dc0e1b382d4e10972
Serabass <serabass565@gmail.com>
2022-04-05 22:22:01 +0600

    upd

--- log.php
+++ log.php
@@ -8,7 +8,8 @@ include_once 'history/history-base.php';
 function getLog()
 {
   $cont = file_get_contents('alisalog.txt');
-  $lines = preg_split('/[\r\n]+/', $cont);
+  $converted = mb_convert_encoding($cont, 'Windows-1251', 'UTF-8'); 
+  $lines = preg_split('/[\r\n]+/', $converted);
   $res = [];
 
   for ($i = 0; $i < count($lines); $i++) {
@@ -20,14 +21,26 @@ function getLog()
 
 $log = getLog();
 
-header('Content-Type: text/html; charset=utf-8');
-echo '<pre>';
-echo '<ul>';
-  foreach ($log as $row) {
-    echo '<li>';
-    echo '<b>' . $row['time'] . '</b>';
-    echo '<span> ' . $row['text']['request']['command'] . '</span>';
-    echo '</li>';
-  }
-echo '</ul>';
-echo '</pre>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Logs</title>
+</head>
+<body>
+  
+  <ul>
+    <?php foreach ($log as $row) { ?>
+      <li>
+        <b> <?php echo $row['time'] ?> </b>
+        <span> <?php echo $row['text']['request']['command'] ?> </span>
+      </li>
+    <?php } ?>
+  </ul>
+
+</body>
+</html>
